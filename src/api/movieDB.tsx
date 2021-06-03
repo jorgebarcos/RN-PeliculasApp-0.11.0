e95f9af379528b87e01a05806c5ea2a4574257af
Jorge Barcos <jorgebarcos88@gmail.com>
2021-06-03 20:53:26 -0300

    Obtener películas en cartelera

---
+++ src/api/movieDB.tsx
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+const movieDB = axios.create({
+  baseURL: 'https://api.themoviedb.org/3/movie',
+  params: {
+    api_key: 'd214c4adf14394c99fcc204b4a33410f',
+    language: 'es-MX',
+  },
+});
+
+export default movieDB;
