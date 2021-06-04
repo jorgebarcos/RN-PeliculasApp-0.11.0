8372d3096e9838558a336fdbb861dcbda447922e
Jorge Barcos <jorgebarcos88@gmail.com>
2021-06-03 21:52:39 -0300

    Mostar el poster de la película

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import {ActivityIndicator, Text, View} from 'react-native';
 import {useMovies} from '../hooks/useMovies';
+import MoviePoster from '../components/MoviePoster';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
 
 const HomeScreen = () => {
   const {peliculasEnCine, isLoading} = useMovies();
+
+  const {top} = useSafeAreaInsets();
 
   if (isLoading) {
     return (
@@ -14,8 +18,8 @@ const HomeScreen = () => {
   }
 
   return (
-    <View>
-      <Text>HomeScreen</Text>
+    <View style={{marginTop: top + 20}}>
+      <MoviePoster movie={peliculasEnCine[4]} />
     </View>
   );
 };
