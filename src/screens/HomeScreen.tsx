fc2a8c5fd4f893531ca5ad7ae6dd2ad48033085b
Jorge Barcos <jorgebarcos88@gmail.com>
2021-06-04 09:17:24 -0300

    Componente HorizontalSlider

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,16 +1,10 @@
 import React from 'react';
-import {
-  ActivityIndicator,
-  Dimensions,
-  View,
-  FlatList,
-  Text,
-  ScrollView,
-} from 'react-native';
+import {ActivityIndicator, Dimensions, View, ScrollView} from 'react-native';
 import {useMovies} from '../hooks/useMovies';
 import MoviePoster from '../components/MoviePoster';
 import {useSafeAreaInsets} from 'react-native-safe-area-context';
 import Carousel from 'react-native-snap-carousel';
+import HorizontalSlider from '../components/HorizontalSlider';
 
 const {width: windowWidth} = Dimensions.get('window');
 
@@ -40,22 +34,13 @@ const HomeScreen = () => {
             renderItem={({item}: any) => <MoviePoster movie={item} />}
             sliderWidth={windowWidth}
             itemWidth={300}
+            inactiveSlideOpacity={0.9}
           />
         </View>
 
         {/* Peliculas populares */}
-        <View style={{height: 260, backgroundColor: 'red'}}>
-          <Text style={{fontSize: 30, fontWeight: 'bold'}}>En cine</Text>
-          <FlatList
-            data={peliculasEnCine}
-            renderItem={({item}: any) => (
-              <MoviePoster movie={item} width={140} height={200} />
-            )}
-            keyExtractor={item => item.id.toString()}
-            horizontal={true}
-            showsHorizontalScrollIndicator={false}
-          />
-        </View>
+
+        <HorizontalSlider title="En cine" movies={peliculasEnCine} />
       </View>
     </ScrollView>
   );
